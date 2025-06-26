969c5db48ba196a508e3ff9fdaaab02b9987697e
knockjoy <yuupi374@gmail.com>
2025-06-26 13:15:28 +0900

    update

--- src/GameServer/main.py
+++ src/GameServer/main.py
@@ -434,6 +434,6 @@ if __name__ == "__main__":
     # loop=asyncio.new_event_loop()
     # asyncio.set_event_loop(loop)
     # loop.run_until_complete(matching_loop())
-    uvicorn.run("main:app", host="0.0.0.0", port=19004, lifespan="on", reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=50107, lifespan="on", reload=True)
     # loop.close()
     pass
